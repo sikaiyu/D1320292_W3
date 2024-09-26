23932a0d6d305458abb739f8112855fd926292d9
sikaiyu <D1320292@o365.fcu.edu.tw>
2024-09-26 11:07:09 +0800

    work

---
+++ 數字交換.py
@@ -0,0 +1,5 @@
+a=int(input("輸入變數a:"))
+b=int(input("輸入變數b:"))
+a,b=b,a
+print("a=",a)
+print("b=",b)
---
+++ 計算矩形面積周長.py
@@ -0,0 +1,6 @@
+l=int(input("請輸入矩形的長:"))
+w=int(input("請輸入矩形的寬:"))
+b=l*w
+a=(l+w)*2
+print("面積:",b)
+print("周長:",a)
